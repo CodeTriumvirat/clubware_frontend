ce1d9ba0e0c47d52503c9bbb3b64a8962c74e035
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2024-03-13 14:25:13 +0100

    Add tabler icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@mantine/core";
+import { IconPacman } from "@tabler/icons-react";
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <p>Welcome to Clubware!</p>
-      <Button variant="filled" className='bg-slate-500'>Awesome Mantine Button with custom Tailwind color</Button>
+      <Button variant='filled' className='bg-slate-500'>
+        Awesome Mantine Button with custom Tailwind color
+      </Button>
+      <IconPacman stroke={2} />
     </main>
   );
 }
