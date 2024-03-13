f4c243c2b5fbf6bb19ce429c38709bbed339e77f
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2024-03-13 15:13:46 +0100

    Setup vitest with utils for react testing library, add welcome test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@mantine/core";
 import { IconPacman } from "@tabler/icons-react";
+import { Welcome } from "@/components/Welcome/Welcome";
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <p>Welcome to Clubware!</p>
+      <Welcome />
       <Button variant='filled' className='bg-slate-500'>
         Awesome Mantine Button with custom Tailwind color
       </Button>
