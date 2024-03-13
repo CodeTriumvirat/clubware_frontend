e098ba4195e9dff2171313ac28c1d30e86258b34
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2024-03-13 15:57:17 +0100

    Add example playwright test

---
+++ app/e2e-tests/example.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/test";
+
+test("User can navigate to homepage", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  const welcomeText = page.locator('text="Welcome to Clubware"');
+  await expect(welcomeText).toHaveText("Welcome to Clubware");
+});
