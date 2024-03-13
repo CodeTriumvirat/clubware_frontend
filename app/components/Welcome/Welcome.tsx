ddf037de6288aed6248b634320f5af4308c8d566
Timo Bickmann <32876602+timobickmann@users.noreply.github.com>
2024-03-13 16:47:50 +0100

    Setup/mantine tailwind testing
    * Configure mantine with tailwind, add import alias
    
    * Add tabler icons
    
    * Setup vitest with utils for react testing library, add welcome test
    
    * Add example playwright test
    
    * Fix issue vitest and playwright sharing test files

---
+++ app/components/Welcome/Welcome.tsx
@@ -0,0 +1,13 @@
+import { Title, Text, Anchor } from "@mantine/core";
+
+export function Welcome() {
+  return (
+    <>
+      <Title ta='center' mt={100}>
+        <Text inherit variant='gradient' gradient={{ from: "pink", to: "yellow" }}>
+          Welcome to Clubware
+        </Text>
+      </Title>
+    </>
+  );
+}
---
+++ app/test-utils/render.tsx
@@ -0,0 +1,9 @@
+import { render as testingLibraryRender } from "@testing-library/react";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "../styles/theme";
+
+export function render(ui: React.ReactNode) {
+  return testingLibraryRender(<>{ui}</>, {
+    wrapper: ({ children }: { children: React.ReactNode }) => <MantineProvider theme={theme}>{children}</MantineProvider>
+  });
+}
