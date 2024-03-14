e80a3e02bb969241333ccbfb7c07cdcc6da58435
Arasch Saffari <hello@arasch-saffari.com>
2024-03-14 16:37:39 +0100

    added appwrite.tsx

--- app/app/appwrite.tsx
+++ app/app/appwrite.tsx
@@ -3,8 +3,8 @@ import { Client, Account } from "appwrite";
 export const client = new Client();
 
 client
-  .setEndpoint("https://api.jslab.dev/v1")
-  .setProject("65f3150b725f23e537c8");
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
 
 export const account = new Account(client);
 export { ID } from "appwrite";
