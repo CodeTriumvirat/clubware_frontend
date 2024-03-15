6f63cdfd4349d102e77787060e3900ae3b39b729
Lukas Karberg <deaderlp@googlemail.com>
2024-03-15 14:01:41 +0100

    Lukas: Re-added Prettier config

--- app/test-utils/render.tsx
+++ app/test-utils/render.tsx
@@ -1,11 +1,11 @@
-import { render as testingLibraryRender } from "@testing-library/react";
-import { MantineProvider } from "@mantine/core";
-import { theme } from "../styles/theme";
+import { render as testingLibraryRender } from '@testing-library/react'
+import { MantineProvider } from '@mantine/core'
+import { theme } from '../styles/theme'
 
 export function render(ui: React.ReactNode) {
-  return testingLibraryRender(<>{ui}</>, {
-    wrapper: ({ children }: { children: React.ReactNode }) => (
-      <MantineProvider theme={theme}>{children}</MantineProvider>
-    ),
-  });
+    return testingLibraryRender(<>{ui}</>, {
+        wrapper: ({ children }: { children: React.ReactNode }) => (
+            <MantineProvider theme={theme}>{children}</MantineProvider>
+        ),
+    })
 }
