c5860a12b3726c16fc46f698bdf4c8dedde87da9
Gentechnik <112762897+Gentechnik@users.noreply.github.com>
2024-03-15 14:32:26 +0100

    Feature/remove appwrite integration (#18)
    * added appwrite.tsx
    
    * added appwrite.tsx
    
    * rename app in utils
    
    * added prettier configfile
    
    ---------
    
    Co-authored-by: Arasch Saffari <hello@arasch-saffari.com>
    Co-authored-by: Arasch Saffari <110110303+arasch-saffari@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom",
-    setupFiles: "./vitest.setup.js",
-    include: "app/**/*.test.tsx",
-  },
-});
+    plugins: [react()],
+    test: {
+        environment: 'jsdom',
+        setupFiles: './vitest.setup.js',
+        include: 'app/**/*.test.tsx',
+    },
+})
