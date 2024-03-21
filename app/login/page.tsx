eee6a684c471c27ac52708c6ef7f5256b39afd60
Lukas Karberg <deaderlp@googlemail.com>
2024-03-21 13:57:25 +0100

    finishing touches on login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link'
 import { login, logout, signup } from './actions'
 import {
     Button,
     Container,
+    Group,
     Paper,
     PasswordInput,
+    Stack,
     TextInput,
     Title,
 } from '@mantine/core'
@@ -13,46 +16,55 @@ export default function Page() {
         <Container size={420} my={40}>
             <Title mb={12}>Log dich ein, Habibi!</Title>
             <Paper withBorder shadow="md" p={30} radius="md" mt="xl">
-                <form>
-                    <TextInput
-                        label="Email"
-                        placeholder="deinemail@example.com"
-                        id="email"
-                        name="email"
-                        type="email"
-                        required
-                    />
-                    <PasswordInput
-                        label="Passwort"
-                        placeholder="Dein Passwort"
-                        mt="md"
-                        id="password"
-                        name="password"
-                        type="password"
-                        required
-                    />
-                    <Button type="submit" fullWidth mt="xl" formAction={login}>
-                        Log in
-                    </Button>
-                    <Button type="submit" fullWidth mt="xl" formAction={signup}>
-                        Sign up
-                    </Button>
-                </form>
-                <form>
-                    <Button variant="filled" size="xs" mt="md">
-                        Passwort vergessen?
-                    </Button>
-                    <Button
-                        type="submit"
-                        formAction={logout}
-                        variant="filled"
-                        size="xs"
-                        mt="md"
-                    >
-                        Logout
-                    </Button>
-                </form>
-                {/* <Text mt="md">Passwort vergessen?</Text> */}
+                <Stack>
+                    <form>
+                        <Stack>
+                            <TextInput
+                                label="Email"
+                                placeholder="deinemail@example.com"
+                                id="email"
+                                name="email"
+                                type="email"
+                                required
+                            />
+                            <PasswordInput
+                                label="Passwort"
+                                placeholder="Dein Passwort"
+                                id="password"
+                                name="password"
+                                type="password"
+                                required
+                            />
+                            <Button type="submit" fullWidth formAction={login}>
+                                Log in
+                            </Button>
+                            <Button type="submit" fullWidth formAction={signup}>
+                                Sign up
+                            </Button>{' '}
+                        </Stack>
+                    </form>
+                    <Group justify="space-between" grow>
+                        <Button
+                            variant="filled"
+                            size="xs"
+                            component={Link}
+                            href="/auth/recovery"
+                        >
+                            Passwort vergessen?
+                        </Button>
+                        <form>
+                            <Button
+                                fullWidth
+                                type="submit"
+                                formAction={logout}
+                                variant="filled"
+                                size="xs"
+                            >
+                                Logout
+                            </Button>
+                        </form>
+                    </Group>
+                </Stack>
             </Paper>
         </Container>
     )
