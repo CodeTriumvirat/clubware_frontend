fff22f3086f1c8bf9176d45ec8ffe49f4acb8b7f
Lukas Karberg <deaderlp@googlemail.com>
2024-03-21 13:15:00 +0100

    finished up bug fix

--- app/components/Profile/Profile.tsx
+++ app/components/Profile/Profile.tsx
@@ -2,83 +2,88 @@ import { Container, Paper, Title, Text, Image, Flex } from '@mantine/core'
 import NextImage from 'next/image'
 import myImage from '/public/images/bg-9.png'
 import { createClient } from '@/utils/supabase/server'
-import { Database } from '@/types/supabase'
-import { UserProfile } from '@/types/types'
+import { NoProfile } from '../NoProfile/NoProfile'
 
 export async function Profile() {
     const supabase = createClient()
 
     let { data, error } = await supabase.from('user_profiles').select('*')
 
-    return data ? (
-        <Container size={420} my={40}>
-            {/* <div>{JSON.stringify(data)}</div> */}
-            <Paper
-                withBorder
-                shadow="md"
-                p={30}
-                radius="md"
-                mt="xl"
-                className="text-black"
-            >
-                <Flex justify="center" direction="column" align="center">
-                    <Title order={2}>User Profile</Title>
-                    <Image
-                        component={NextImage}
-                        src={myImage}
+    return (
+        <>
+            {data && (
+                <Container size={420} my={40}>
+                    <Paper
+                        withBorder
+                        shadow="md"
+                        p={30}
                         radius="md"
-                        alt="My image"
-                        w="80%"
-                        h={150}
-                        fit="contain"
-                        mt="md"
-                    />
-                    <Flex direction="column" gap="sm" mt="md" w="80%">
+                        mt="xl"
+                        className="text-black"
+                    >
                         <Flex
-                            gap="md"
-                            justify="space-between"
-                            align="flex-start"
-                            direction="row"
-                            wrap="wrap"
+                            justify="center"
+                            direction="column"
+                            align="center"
                         >
-                            <Text>Firstname</Text>
-                            <Text>{data[0].first_name}</Text>
+                            <Title order={2}>User Profile</Title>
+                            <Image
+                                component={NextImage}
+                                src={myImage}
+                                radius="md"
+                                alt="My image"
+                                w="80%"
+                                h={150}
+                                fit="contain"
+                                mt="md"
+                            />
+                            <Flex direction="column" gap="sm" mt="md" w="80%">
+                                <Flex
+                                    gap="md"
+                                    justify="space-between"
+                                    align="flex-start"
+                                    direction="row"
+                                    wrap="wrap"
+                                >
+                                    <Text>Firstname</Text>
+                                    <Text>{data[0].first_name}</Text>
+                                </Flex>
+                                <Flex
+                                    gap="md"
+                                    justify="space-between"
+                                    align="flex-start"
+                                    direction="row"
+                                    wrap="wrap"
+                                >
+                                    <Text>Lastname</Text>
+                                    <Text>{data[0].last_name}</Text>
+                                </Flex>
+                                <Flex
+                                    gap="md"
+                                    justify="space-between"
+                                    align="flex-start"
+                                    direction="row"
+                                    wrap="wrap"
+                                >
+                                    <Text>Birthday</Text>
+                                    <Text>{data[0].date_of_birth}</Text>
+                                </Flex>
+                                <Flex
+                                    gap="md"
+                                    justify="space-between"
+                                    align="flex-start"
+                                    direction="row"
+                                    wrap="wrap"
+                                >
+                                    <Text>Phone</Text>
+                                    <Text>{data[0].phone_number}</Text>
+                                </Flex>
+                            </Flex>
                         </Flex>
-                        <Flex
-                            gap="md"
-                            justify="space-between"
-                            align="flex-start"
-                            direction="row"
-                            wrap="wrap"
-                        >
-                            <Text>Lastname</Text>
-                            <Text>{data[0].last_name}</Text>
-                        </Flex>
-                        <Flex
-                            gap="md"
-                            justify="space-between"
-                            align="flex-start"
-                            direction="row"
-                            wrap="wrap"
-                        >
-                            <Text>Birthday</Text>
-                            <Text>{data[0].date_of_birth}</Text>
-                        </Flex>
-                        <Flex
-                            gap="md"
-                            justify="space-between"
-                            align="flex-start"
-                            direction="row"
-                            wrap="wrap"
-                        >
-                            <Text>Phone</Text>
-                            <Text>{data[0].phone_number}</Text>
-                        </Flex>
-                    </Flex>
-                </Flex>
-            </Paper>
-        </Container>
-    ) : (
-        ''
+                    </Paper>
+                </Container>
+            )}
+            {!data && <NoProfile />}
+        </>
     )
 }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,9 +7,9 @@ export default async function Page() {
 
     let { data, error } = await supabase.auth.getUser()
 
-    if (error || !data?.user) {
-        redirect('/login')
-    }
+    // if (error || !data?.user) {
+    //     redirect('/login')
+    // }
 
     return <Profile />
 }
