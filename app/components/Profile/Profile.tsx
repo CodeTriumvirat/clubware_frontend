c50eb7850d6f618ad43f6e91f30b87809b6f16b9
Lukas Karberg <deaderlp@googlemail.com>
2024-03-22 10:57:55 +0100

    refactored Profile component to use Stack and Group

--- app/components/Profile/Profile.tsx
+++ app/components/Profile/Profile.tsx
@@ -1,4 +1,13 @@
-import { Container, Paper, Title, Text, Image, Flex } from '@mantine/core'
+import {
+    Container,
+    Paper,
+    Title,
+    Text,
+    Image,
+    Flex,
+    Stack,
+    Group,
+} from '@mantine/core'
 import NextImage from 'next/image'
 import myImage from '/public/images/bg-9.png'
 import { createClient } from '@/utils/supabase/server'
@@ -14,11 +23,7 @@ export async function Profile() {
             {data && (
                 <Container size={420} my={40}>
                     <Paper withBorder shadow="md" p={30} radius="md" mt="xl">
-                        <Flex
-                            justify="center"
-                            direction="column"
-                            align="center"
-                        >
+                        <Stack justify="center" align="center">
                             <Title order={2}>User Profile</Title>
                             <Image
                                 component={NextImage}
@@ -30,49 +35,45 @@ export async function Profile() {
                                 fit="contain"
                                 mt="md"
                             />
-                            <Flex direction="column" gap="sm" mt="md" w="80%">
-                                <Flex
+                            <Stack gap="sm" mt="md" w="80%">
+                                <Group
                                     gap="md"
                                     justify="space-between"
                                     align="flex-start"
-                                    direction="row"
                                     wrap="wrap"
                                 >
                                     <Text>Firstname</Text>
                                     <Text>{data[0].first_name}</Text>
-                                </Flex>
-                                <Flex
+                                </Group>
+                                <Group
                                     gap="md"
                                     justify="space-between"
                                     align="flex-start"
-                                    direction="row"
                                     wrap="wrap"
                                 >
                                     <Text>Lastname</Text>
                                     <Text>{data[0].last_name}</Text>
-                                </Flex>
-                                <Flex
+                                </Group>
+                                <Group
                                     gap="md"
                                     justify="space-between"
                                     align="flex-start"
-                                    direction="row"
                                     wrap="wrap"
                                 >
                                     <Text>Birthday</Text>
                                     <Text>{data[0].date_of_birth}</Text>
-                                </Flex>
-                                <Flex
+                                </Group>
+                                <Group
                                     gap="md"
                                     justify="space-between"
                                     align="flex-start"
-                                    direction="row"
                                     wrap="wrap"
                                 >
                                     <Text>Phone</Text>
                                     <Text>{data[0].phone_number}</Text>
-                                </Flex>
-                            </Flex>
-                        </Flex>
+                                </Group>
+                            </Stack>
+                        </Stack>
                     </Paper>
                 </Container>
             )}
