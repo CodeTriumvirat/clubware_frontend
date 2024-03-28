dc9b73199025be2295ef3c7293fb5d2846ebfbdb
Lukas Karberg <deaderlp@googlemail.com>
2024-03-28 17:05:20 +0100

    finished up on some cleanup

--- app/profile/actions.ts
+++ app/profile/actions.ts
@@ -2,13 +2,11 @@
 
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-
 import { createClient } from '@/_utils/supabase/server'
 import { UserProfile } from '@/_types'
 
 export async function updateUserProfile(profileData: UserProfile) {
     const supabase = createClient()
-
     const { error } = await supabase
         .from('user_profile')
         .update({ ...profileData })
@@ -18,19 +16,16 @@ export async function updateUserProfile(profileData: UserProfile) {
         revalidatePath('/profile', 'layout')
         redirect('/profile')
     }
-
     if (error) throw new Error('Failed to update profile')
 }
 export async function uploadProfilePicture(
     formData: FormData,
     user_id: string
 ) {
-    // throw new Error('Failed to upload profile picture')
     const supabase = createClient()
     const file = formData.get('file') as File
     if (file) {
         const fileType = file.type.split('/')[1]
-        console.log(fileType)
         const { error } = await supabase.storage
             .from('user_data')
             .upload(`${user_id}/profile_picture.${fileType}`, file, {
@@ -38,15 +33,10 @@ export async function uploadProfilePicture(
             })
         if (error) {
             throw new Error('Failed to upload profile picture')
-            // console.error('Error uploading file:', error.message)
         } else {
             console.log('Upload successful')
         }
     } else {
         console.log('No file provided for upload')
     }
-}
-
-export async function rawFunction2() {
-    throw new Error('Raw function 2 throwing error')
 }
