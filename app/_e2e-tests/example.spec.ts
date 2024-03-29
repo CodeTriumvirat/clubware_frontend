d46d85bd61f9d5fb94cccae0aa32bf51c2d64df3
Lukas Karberg <deaderlp@googlemail.com>
2024-03-29 17:11:29 +0100

    added small playwright testing

--- app/_e2e-tests/example.spec.ts
+++ app/_e2e-tests/example.spec.ts
@@ -1,7 +1,18 @@
 import { test, expect } from '@playwright/test'
 
 test('User can navigate to homepage', async ({ page }) => {
-    await page.goto('http://localhost:3000/')
-    const welcomeText = page.locator('text="Welcome to Clubware"')
-    await expect(welcomeText).toHaveText('Welcome to Clubware')
+    await page.goto('http://localhost:3010/')
+    const welcomeText = page.locator('text="Log dich ein, Habibi!"')
+    await expect(welcomeText).toHaveText('Log dich ein, Habibi!')
+})
+
+test('User can log in', async ({ page }) => {
+    await page.goto('http://localhost:3010/')
+    await page.getByPlaceholder('deinemail@example.com').click()
+    await page.getByPlaceholder('deinemail@example.com').fill('test@mail.de')
+    await page.getByPlaceholder('Dein Passwort').click()
+    await page.getByPlaceholder('Dein Passwort').fill('@MeinPasswort1')
+    await page.getByRole('button', { name: 'Log in' }).click()
+    const welcomeText = page.locator('text="Welcome"')
+    await expect(welcomeText).toHaveText('Welcome')
 })
