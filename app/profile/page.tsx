5e0345b7856071fb0e049e96bbfbcbf01dd24c5e
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2024-03-29 10:17:59 +0100

    Add middleware auth

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,15 +1,5 @@
-import { redirect } from 'next/navigation'
-import { createClient } from '@/_utils/supabase/server'
 import { ProfileCard } from '@/profile/ProfileCard'
 
 export default async function Page() {
-    const supabase = createClient()
-
-    let { data, error } = await supabase.auth.getUser()
-
-    if (error || !data?.user) {
-        redirect('/login')
-    }
-
     return <ProfileCard />
 }
--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,18 @@
-import { type NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 import { updateSession } from '@/_utils/supabase/middleware'
+import { createClient } from '@/_utils/supabase/server'
 
 export async function middleware(request: NextRequest) {
-    return await updateSession(request)
+    const supabase = createClient()
+    const url = request.nextUrl.clone()
+    const response = await updateSession(request)
+    const { error } = await supabase.auth.getUser()
+    if (error) {
+        url.pathname = '/login'
+        return NextResponse.redirect(url)
+    }
+
+    return response
 }
 
 export const config = {
@@ -12,8 +22,10 @@ export const config = {
          * - _next/static (static files)
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
+         * - /login (login route)
+         * - /confirm (confirm route)
          * Feel free to modify this pattern to include more paths.
          */
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|login|confirm|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
