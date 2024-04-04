3961f3698e5a51357cd880a6e686d097bda8fab2
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2024-04-04 11:20:56 +0200

    Fix nav typo

--- app/_components/AppContainer/nav-data.ts
+++ app/_components/AppContainer/nav-data.ts
@@ -24,7 +24,7 @@ export const navData = [
         ],
     },
     {
-        label: 'inventory',
+        label: 'Inventory',
         icon: IconBuildingWarehouse,
         links: [
             { label: 'Overview', link: '/inventory' },
