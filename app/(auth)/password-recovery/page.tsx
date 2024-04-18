1d2dc4113106cae42afe4feccd0c428d6d842af7
Lukas Karberg <deaderlp@googlemail.com>
2024-04-18 11:47:46 +0200

    reverted extra Password Recovery Page

--- app/(auth)/password-recovery/page.tsx
+++ app/(auth)/password-recovery/page.tsx
@@ -5,6 +5,10 @@ import { redirect } from 'next/navigation'
 export default async function Page() {
     const supabase = createClient()
 
+    const { error } = await supabase.auth.getUser()
+
+    if (error) redirect('/')
+
     return (
         <>
             <PasswordRecoveryForm />
